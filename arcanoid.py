dffe1272ee480e920246e692fc7e8fec84e1dc31
LeoGidev <leogimenez.dev@gmail.com>
2024-03-23 18:01:51 -0300

    se agregan inicializaciones

--- arcanoid.py
+++ arcanoid.py
@@ -1,2 +1,26 @@
 import pygame
 import random
+# Inicialización de Pygame
+pygame.init()
+
+# Dimensiones de la pantalla
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Colores
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+# Velocidad de la pelota
+BALL_SPEED = 5
+
+# Dimensiones de la paleta
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 20
+
+# Dimensiones de los bloques
+BLOCK_WIDTH = 60
+BLOCK_HEIGHT = 30
+BLOCK_ROWS = 5
+BLOCK_COLUMNS = 10
